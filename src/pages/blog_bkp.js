dd641b27ba03d97718f5338fca984759b3c3f74a
Akash <cole.akash@Akash.local>
2021-05-06 11:08:54 +0530

    fix: Build issues and GraphQL issue fixed

--- src/pages/blog_bkp.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-import BlogLogin from "../components/BlogLogin"
-
-export default () => (
-  <Layout>
-    <BlogLogin />
-  </Layout>
-);
--- src/pages/talks-and-pods_bkp.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-import TalksLogin from "../components/TalksLogin"
-
-export default () => (
-  <Layout>
-    <TalksLogin />
-  </Layout>
-);
