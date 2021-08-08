6372531435f3f89eb5bf0a29ffd6a8a49c17ea00
AKASH KAINTURA <akashkaintura@yahoo.com>
2021-08-08 16:18:35 +0530

    Delete portfolio.js

--- src/pages/portfolio.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-import Portfolio from '../components/Portfolio'
-
-export default () => (
-  <Layout>
-    <Portfolio />
-  </Layout>
-)
