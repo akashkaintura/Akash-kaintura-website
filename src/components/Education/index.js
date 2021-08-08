9b4eeb65f7496a173e392660e0316486c6a6b7ba
AKASH KAINTURA <akashkaintura@yahoo.com>
2021-08-08 18:28:39 +0530

    Update index.js

--- src/components/Education/index.js
+++ src/components/Education/index.js
@@ -6,16 +6,12 @@ export default (props) => (
       <strong>Uttaranchal University*</strong>
     </p>
     <p> August 2013 - May 2016</p>
-    <p>
-      Masters of Computer Applications
-      {/* <br /> Specialization in Salesforce, Python
-       */}
-    </p>
+    <p>Bachelors in Computer Applications</p>
     <p>
       <strong>Uttaranchal University*</strong>
     </p>
     <p> Septemeber 2016 - May 2018</p>
-    <p>Bachelors in Computer Applications</p>
+     <p> Masters of Computer Applications</p>
     <hr />
   </div>
 )
